cce70b3018b37b0160a50ff75dbd094e90fb126c
chashoff <crhoffman@student.fullsail.edu>
2018-11-26 22:53:56 -0500

    Pushed first assignment.

---
+++ ChaseHoffman_CE01/ChaseHoffman_CE01/CheckingAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChaseHoffman_CE01
+{
+    class CheckingAccount
+    {
+        public decimal accBal;
+        public int accNum;
+
+        public CheckingAccount(int intp, decimal decp)
+        {
+            accNum = intp;
+            accBal = decp;
+            Console.WriteLine($"Your Account Number is: {accNum}");
+            Console.WriteLine($"Your Account Balance is: {accBal.ToString("c")}");
+        }
+
+        public void setBal(decimal decp)
+        {
+            accBal = decp;
+        }
+        public void setAcc(int intp)
+        {
+            accNum = intp;
+        }
+    }
+}
---
+++ ChaseHoffman_CE01/ChaseHoffman_CE01/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChaseHoffman_CE01
+{
+    class Menu
+    {
+        public static void Gui()
+        {
+ 
+            Console.WriteLine("[1] Create Customer");
+            Console.WriteLine("[2] Create Account");
+            Console.WriteLine("[3] Set Account Balance");
+            Console.WriteLine("[4]  Display Account Balance");
+            Console.WriteLine("");
+            Console.WriteLine("[0] Exit");
+            Console.WriteLine("Please Type Your Selection...");
+        }
+    }
+}
